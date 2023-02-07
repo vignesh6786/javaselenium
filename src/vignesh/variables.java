c7927ad1fdd9604aeb827f69bb495ed14a83a82c
vignesh6786 <vignesh6786@gmail.com>
2023-02-07 17:05:50 +0530

    check

---
+++ src/vignesh/variables.java
@@ -0,0 +1,33 @@
+package vignesh;
+
+public class variables {
+
+	public static void main(String[] args) {
+		
+  int a,b;
+  a=5;b=9;
+  System.out.println(a+b);
+  
+  
+   float n,m;
+   n=8.3f;m=5.6f;
+   System.out.println(n+m);
+   
+   boolean option=true;
+   System.out.println(option);
+   
+   String email="rajaraman.vkr@gmail.com";
+   System.out.println(email);
+   
+   char male='M';
+   System.out.println(male);
+   
+   long phoneno=9787878722222l;
+   System.out.println(phoneno);
+   
+   double avgsalary=76588.56766;
+   System.out.println(avgsalary);
+		  
+	}
+
+}
